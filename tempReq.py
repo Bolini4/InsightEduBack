d2daf2931eb2a3df5fa382fc90c32ee528ceea99
Bolini <matteo.bolin@gmail.com>
2023-12-02 10:11:14 +0100

    login @jwt_required ok

--- tempReq.py
+++ tempReq.py
@@ -5,6 +5,7 @@ print("Press 2 for logout test")
 print("Press 3 for jwt required test")
 choice = int(input("1 or 2 or 3"))
 
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwMTUwODE1OCwianRpIjoiYTYzZjEwZDMtYWMzYi00NzZjLWJmMmYtMGY1OTlkNmQxMWZiIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6Im1hdHRlb2JvbGluaUBnbWFpbC5jb20iLCJuYmYiOjE3MDE1MDgxNTgsImV4cCI6MTcwMTUxMTc1OH0.ewXUudt_swsJbgVQcjybcgawpG7aVtP7J61_43UZ9Zs"
 if choice == 1:
     
         url = "http://127.0.0.1:5000/auth/login"
@@ -18,12 +19,14 @@ elif choice == 2:
                 'password':'hashedpass'}
         req = r.post(url,data)
         print(req.text)
-
 elif choice == 3:
+        headers = {
+    "Authorization": f"Bearer {token}"
+}
         url = "http://127.0.0.1:5000/"
         data = {'email':"matteobolini@gmail.com",
                 'password':'hashedpass'}
-        req = r.post(url,data)
+        req = r.post(url,data, headers=headers)
         print(req.text)           
 
 input("press enter to exit")
