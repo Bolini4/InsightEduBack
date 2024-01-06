74a2d4d6da9f8a2fe955964e65ce920b58f7c83d
Bolini <matteo.bolin@gmail.com>
2024-01-06 10:30:33 +0100

    some tries with tokens

--- tempReq.py
+++ tempReq.py
@@ -5,7 +5,9 @@ print("Press 2 for logout test")
 print("Press 3 for jwt required test")
 choice = int(input("1 or 2 or 3"))
 
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwMTU5NTQzOSwianRpIjoiNWVjZTgzYTgtMjE1OS00MmYyLTlmZTgtOGFmZDVhMTgwZjBkIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6Im1hdHRlb2JvbGluaUBnbWFpbC5jb20iLCJuYmYiOjE3MDE1OTU0MzksImV4cCI6MTcwMTU5OTAzOX0.YhYLpnCrm5jmUsLgo8DqFadQ4cPtu4IVwf17diFQDjo"
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwNDUzMjQ2MCwianRpIjoiZTI2ZTg0NDMtZTZhNS00MGQ2LTgzMzEtM2FjNzMzNDUwNDlkIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6Im1hdHRlb2JvbGluaUBnbWFpbC5jb20iLCJuYmYiOjE3MDQ1MzI0NjAsImV4cCI6MTcwNDUzNjA2MH0.Hg1ISZYt0WiefqCwvZUXCTmItXs7tnEAHUk87ZQDYy8"
+
+
 if choice == 1:
     
         url = "http://127.0.0.1:5000/auth/login"
@@ -27,8 +29,8 @@ elif choice == 3:
     "Authorization": f"Bearer {token}"
 }
         url = "http://127.0.0.1:5000/"
-        data = {'email':"matteobolini@gmail.com",
-                'password':'hashedpass'}
+        data = {'email':"bolinettemail@google.com",
+                'password':'password'}
         req = r.post(url,data, headers=headers)
         print(req.text)           
 
